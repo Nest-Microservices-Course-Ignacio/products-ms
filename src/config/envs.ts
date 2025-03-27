c6109e886170a5775c6b879f4b04233e65a09d5e
nachomartinez3010 <nachomartinez3010@gmail.com>
2025-03-27 17:08:12 -0600

    add Prisma setup with SQLite and create Product table; update environment configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,11 +3,13 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  DATABASE_URL: string;
 }
 
 const envVarsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().default(3000).optional(),
+    DATABASE_URL: joi.string().required().default('file:./dev.db'),
   })
   .unknown(true)
   .required();
@@ -24,4 +26,5 @@ if (error) {
 export const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
+  databaseUrl: envVars.DATABASE_URL,
 };
